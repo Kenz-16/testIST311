0015c71fee0385645a6e3b7696241c928eac940c
Kenz-16 <mcp5314@psu.edu>
2018-04-10 14:07:16 -0400

    Test creating a repo and pushing a project

--- src/ist311test/IST311Test.java
+++ src/ist311test/IST311Test.java
@@ -15,7 +15,7 @@ public class IST311Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String test= "Hello";
     }
     
 }
